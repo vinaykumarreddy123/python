ada2977209ef80c3310a09d01427594923bccdac
vinaykumarreddy123 <112843667+vinaykumarreddy123@users.noreply.github.com>
2022-09-20 10:54:18 +0530

    Add files via upload

---
+++ loaves of bread problem.py
@@ -0,0 +1,8 @@
+no= int(input ("enter the number"))
+print ('Loaves Discount')
+tmp=no*185;
+print("Regular Price",tmp);
+tmp2=no*185*0.6;
+print("Total Discount",tmp2);
+amount=tmp-tmp2
+print("Total Amount to be paid",amount);
